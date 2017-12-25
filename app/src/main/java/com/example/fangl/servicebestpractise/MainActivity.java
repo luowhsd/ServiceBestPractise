5ea3749fd81e932e15e30f19469fbd879126b517
luowhsd <591151328@qq.com>
2017-12-25 21:54:39 +0800

    20171225

--- app/src/main/java/com/example/fangl/servicebestpractise/MainActivity.java
+++ app/src/main/java/com/example/fangl/servicebestpractise/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.fangl.servicebestpractise;
 
+import android.Manifest;
 import android.content.ComponentName;
+import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -33,20 +40,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startDownload.setOnClickListener(this);
         pauseDownload.setOnClickListener(this);
         cancelDownload.setOnClickListener(this);
+        Intent intent = new Intent(this,DownloadService.class);
+        startService(intent);
+        bindService(intent,connection,BIND_AUTO_CREATE);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }
 
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length>0 && grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MainActivity.this,"拒绝权限将无法使用程序",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+
+                break;
+            default:
+        }
+    }
+
+    @Override
     public void onClick(View v) {
+
+        if(downloadBinder==null)
+            return;
+
         switch (v.getId()){
             case R.id.start_download:
+                String url = "http://120.76.46.60:8001/download/kkcs_eapapp.apk";
+                downloadBinder.startDownload(url);
                 break;
             case R.id.pause_download:
+                downloadBinder.pauseDownload();
                 break;
             case R.id.cancel_download:
+                downloadBinder.cancelDownload();
                 break;
             default:
 
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(connection);
     }
 }
